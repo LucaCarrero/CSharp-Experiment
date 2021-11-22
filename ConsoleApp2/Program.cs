a0d7d108b1fc50ec85c86fea575eca5b80b9838a
Luca <oooiii891@gmail.com>
2021-11-22 21:17:32 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp2
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            test t1 = new test() {Id =1 , Hint ="", Name="" };
+
+            List<string> f = new List<string>()
+            {
+              
+                "Hint"
+            };
+            
+            var type = LinqRuntimeTypeBuilder.genereteNewType(t1, f);
+
+            using (var db = new EagerLoadingDbContext())
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                // the code that you want to measure comes here                  
+
+
+                var p = SelectDynamic(db.demo.AsQueryable(), type.Item1, type.Item2).ToList();
+
+                //var p = db.demo.Select(el => new {hit =  el.Hint}).ToList();
+
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+            }
+        }
+
+
+        public static IQueryable<dynamic> SelectDynamic<T>(IQueryable<T> source, Type dynamicNewType, Dictionary<string, PropertyInfo> sourceProperties)
+        {
+
+            ParameterExpression sourceItem = Expression.Parameter(source.ElementType, "t");
+            IEnumerable<MemberBinding> bindings = dynamicNewType.GetFields().Select(p => 
+                    Expression.Bind(
+                        p, 
+                        Expression.Property(sourceItem, sourceProperties[p.Name])
+                    )).OfType<MemberBinding>();
+
+            var selector = Expression.Lambda<Func<T, dynamic>>(Expression.MemberInit(
+                Expression.New(dynamicNewType.GetConstructor(Type.EmptyTypes)), bindings), sourceItem).Compile();
+
+            return source.Select(selector).AsQueryable();
+        }
+    }
+
+    class EagerLoadingDbContext : DbContext
+    {
+
+        private const string connectionString = "Data Source=c:\\sqlite.db";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString);
+        }
+
+        public DbSet<test> demo { get; set; }
+
+    }
+
+    public class test
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Hint { get; set; }
+
+        public string c;
+    }
+}
---
+++ ConsoleApp2/SelectBuilder.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    public static class LinqRuntimeTypeBuilder
+    {
+       
+        private static AssemblyName assemblyName = new AssemblyName() { Name = "DynamicLinqTypes" };
+        private static ModuleBuilder moduleBuilder = null;
+        private static Dictionary<string, Type> builtTypes = new Dictionary<string, Type>();
+
+        static LinqRuntimeTypeBuilder()
+        {
+            var newTypeName = Guid.NewGuid().ToString();
+            var assemblyName = new AssemblyName(newTypeName);
+            var dynamicAssembly = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+
+            moduleBuilder = dynamicAssembly.DefineDynamicModule("Main");
+        }
+
+        private static string GetTypeKey(Dictionary<string, Type> fields)
+        {
+            //TODO: optimize the type caching -- if fields are simply reordered, that doesn't mean that they're actually different types, so this needs to be smarter
+            string key = string.Empty;
+            foreach (var field in fields)
+                key += field.Key + ";" + field.Value.Name + ";";
+
+            return key;
+        }
+
+        public static Type GetDynamicType(Dictionary<string, Type> fields)
+        {
+            if (null == fields)
+                throw new ArgumentNullException("fields");
+            if (0 == fields.Count)
+                throw new ArgumentOutOfRangeException("fields", "fields must have at least 1 field definition");
+
+            try
+            {
+                Monitor.Enter(builtTypes);
+                string className = GetTypeKey(fields);
+
+                if (builtTypes.ContainsKey(className))
+                    return builtTypes[className];
+
+                TypeBuilder typeBuilder = moduleBuilder.DefineType(className, TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.Serializable);
+
+                foreach (var field in fields)
+                    typeBuilder.DefineField(field.Key, field.Value, FieldAttributes.Public);
+
+                builtTypes[className] = typeBuilder.CreateType();
+
+                return builtTypes[className];
+            }
+            catch (Exception ex)
+            {
+     
+            }
+            finally
+            {
+                Monitor.Exit(builtTypes);
+            }
+
+            return null;
+        }
+
+
+        private static string GetTypeKey(IEnumerable<PropertyInfo> fields)
+        {
+            return GetTypeKey(fields.ToDictionary(f => f.Name, f => f.PropertyType));
+        }
+
+        public static Type GetDynamicType(IEnumerable<PropertyInfo> fields)
+        {
+            return GetDynamicType(fields.ToDictionary(f => f.Name, f => f.PropertyType));
+        }
+
+
+        public static (Type, Dictionary<string, PropertyInfo>) genereteNewType<T>(T source, IEnumerable<string> fieldNames)
+        {
+            Dictionary<string, PropertyInfo> sourceProperties = fieldNames.ToDictionary(name => name, name => source.GetType().GetProperty(name));
+            Type dynamicType = LinqRuntimeTypeBuilder.GetDynamicType(sourceProperties.Values);
+
+            return (dynamicType, sourceProperties);
+        }
+    }
+}
